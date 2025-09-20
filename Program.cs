b00f10c182a0f95bc9241ec0f7b0b2214485428e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-09-19 22:08:03 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Peruibe");
+Console.WriteLine("Itanhaem");
+Console.WriteLine("Mongagua");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Sao Vicente");
+Console.WriteLine("Santos");
+Console.WriteLine("Guaruja");
